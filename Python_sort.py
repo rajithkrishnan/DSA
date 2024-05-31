ee94256b184df648ecef9c494dc98f98bfed7bf1
Rajith Krishnan <iamrajithkrishnan@gmail.com>
2024-05-31 17:05:17 +0530

    Create Python_sort.py

---
+++ Python_sort.py
@@ -0,0 +1,8 @@
+# Get array from user input
+arr = list(map(int, input("Enter elements of the array separated by space: ").split()))
+
+# Sort the array
+sorted_arr = sorted(arr)
+
+# Print the sorted array
+print("Sorted array:", sorted_arr)
